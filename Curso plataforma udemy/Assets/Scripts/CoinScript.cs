c1bf3cc979eb194693777edcf2b571bb9a5c50c9
Luiz Felipe R <luizfelipers19@gmail.com>
2017-08-31 02:41:41 -0300

    4o Commit
    Adição de UI de Coins
    Contador funcionando.
    Próximo passo: criar UI de Health

---
+++ Curso plataforma udemy/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+    private LevelManager theLevelManager; //declaração da variável theLevelManager do tipo Level Manager, para poder acessar seus atributos
+                                            //e métodos
+
+    public int coinValue; //valor inteiro referente ao valor que cada moeda terá. diferentes tipos de moeda podem ter dif valores
+
+
+	// Use this for initialization
+	void Start () {
+        theLevelManager = FindObjectOfType<LevelManager>();//associa o objeto do tipo Level Manager á variável theLevelManager
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)//quando algo entra no colisor da moeda
+    {
+       if(collision.tag == "Player") //checa se o objeto colisor tem a tag Player
+        {
+            theLevelManager.AddCoins(coinValue);//aciona o método AddCoins do script LevelManager. Com isso a soma do valor da moeda com a pontuação atual é realizada
+            Destroy(gameObject);//Destroi o gameObject da moeda na hierarquia do jogo
+
+        }
+    }
+}
--- Curso plataforma udemy/Assets/Scripts/LevelManager.cs
+++ Curso plataforma udemy/Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 
@@ -8,14 +9,16 @@ public class LevelManager : MonoBehaviour {
     public PlayerController thePlayer; // variável do tipo PlayerController, que será usada para achar o gameObject.
     public float waitToDie;//valor referente ao atraso para o jogador sair de cena quando morre
 
-    public GameObject deathExplosion;
+    public GameObject deathExplosion; //gameObject referente ao Particle Systems de sangue, quando o Player morre
 
+    public int coinCount;//variavel responsavel por guardar a pontuação do jogador
 
+    public Text coinText;
 
 	// Use this for initialization
 	void Start () {
         thePlayer = FindObjectOfType<PlayerController>(); // Encontra o componente PlayerController (script), e com isso, retorna o único objeto que está ligado nele.
-        
+        coinText.text = "Coins: 0";
 
     }
 	
@@ -45,6 +48,11 @@ public class LevelManager : MonoBehaviour {
 
         thePlayer.gameObject.SetActive(true); //liga o jogador
 
+    }
+    public void AddCoins(int coinsToAdd)
+    {
+        coinCount += coinsToAdd;
+        coinText.text = "Coins: " + coinCount;//Atualiza o elemento de UI text, de modo que apareç
     }
 
 
